40ff8432aa21d98b00d3743f5d400fd6010e50de
EmnaTay <125699761+EmnaTay@users.noreply.github.com>
2024-09-02 01:20:44 +0400

    changes to .rst

--- docs/source/9-dashboard/dashboard-generating-scripts/proccessing_con_files_for_table.py
+++ docs/source/9-dashboard/dashboard-generating-scripts/proccessing_con_files_for_table.py
@@ -216,13 +216,18 @@ import glob
 import plotly.graph_objs as go
 
 fig = go.Figure()
-con_files = glob.glob(r"data")
+con_files = glob.glob(r"data/*.con")
 for con_file in con_files:
-    raw = mne.io.read_raw_ctf(con_file, preload=True)
-    raw.filter(1.0, 40.0, fir_design="firwin")
-    psds, freqs = mne.time_frequency.psd_welch(raw, fmin=1.0, fmax=40.0, n_fft=2048)
+    try:
+        raw = mne.io.read_raw_ctf(con_file, preload=True)
+        raw.filter(1.0, 40.0, fir_design="firwin")
+        psds, freqs = mne.time_frequency.psd_welch(raw, fmin=1.0, fmax=40.0, n_fft=2048)
 
-    fig.add_trace(go.Scatter(x=freqs, y=psds.mean(axis=0), mode="lines", name=con_file))
+        fig.add_trace(
+            go.Scatter(x=freqs, y=psds.mean(axis=0), mode="lines", name=con_file)
+        )
+    except Exception as e:
+        print(f"Error processing {con_file}: {e}")
 
 fig.update_layout(
     title="FFT Plots for Multiple .con Files",
